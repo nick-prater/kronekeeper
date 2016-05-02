9b8afc61a6f39fd8537a7ec638f4701d6b0f3a80
Nick Prater <nick@npbroadcast.com>
2016-05-02 19:25:36 +0100

    Initial Commit, based on Dancer2 skeleton app

---
+++ lib/kronekeeper.pm
@@ -0,0 +1,41 @@
+package kronekeeper;
+
+=head1 LICENCE
+
+This file is part of Kronekeeper, a web based application for 
+recording and managing wiring frame records.
+
+Copyright (C) 2016 NP Broadcast Limited
+
+Kronekeeper is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kronekeeper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with GOALS-logger.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+use strict;
+use warnings;
+use Dancer2;
+use kronekeeper::Frame;
+
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+
+
+
+
+1;
---
+++ lib/kronekeeper/Frame.pm
@@ -0,0 +1,53 @@
+package kronekeeper::Frame;
+
+=head1 LICENCE
+
+This file is part of Kronekeeper, a web based application for 
+recording and managing wiring frame records.
+
+Copyright (C) 2016 NP Broadcast Limited
+
+Kronekeeper is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kronekeeper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with GOALS-logger.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+
+
+use strict;
+use warnings;
+use Dancer2 appname => 'kronekeeper';
+use Dancer2::Plugin::Database;
+
+prefix '/frame';
+
+get '/' => sub {
+
+	my $q = database->prepare("SELECT * FROM frame");
+	$q->execute;
+	my $f = $q->fetchrow_hashref;
+
+	return sprintf(
+		"frame %u::%s",
+		$f->{id},
+		$f->{name},
+	);
+	
+};
+
+
+
+
+
+
+1;
