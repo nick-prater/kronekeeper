b97be0bf78a317787666deaf6724c7c973dc6a2c
Nick Prater <nick@npbroadcast.com>
2016-08-09 19:59:47 +0100

    Frame view - per-block context menu in place

--- public/js/frame.js
+++ public/js/frame.js
@@ -28,29 +28,120 @@ require([
 ) {
         'use strict';
 
-	$("#block_menu").menu();
+	/* Keep track of the block we are associated with */
+	var jq_block = null;
+	
+
+	/* Initialise the menu and associated events */
+	$("#block_menu").menu({
+		select: handle_menu_selection
+	});
 	$(".block .menu_button a").on("click", show_block_menu);
 
 
-
 	function show_block_menu(e) {
-		console.log("show block menu");
+
+		e.stopPropagation();
+
+		/* Set global block_id - which block opened the menu? */
+		jq_block = $(e.target).closest("td");
+
+		set_allowed_menu_options( );
 		$("#block_menu").menu().show().position({
 			my: "left top",
 			at: "left bottom",
-			of: this
+			of: this,
+			collision: "fit flip"
 		});
 
 		/* Clicking outside the menu closes it */
 		$(document).on("click", function() {
 			$("#block_menu").menu().hide();
 		});
-
-		/* Stop this event propogating down to the document */
-		return false;
 	};
 
-	
+
+	function set_allowed_menu_options() {
+
+		enable_menu_action_if_true(
+			"open",
+			!jq_block.hasClass("is_free") && !jq_block.hasClass("unavailable")
+		);
+		enable_menu_action_if_true(
+			"place",
+			jq_block.hasClass("is_free") && !jq_block.hasClass("unavailable")
+		);
+		enable_menu_action_if_true(
+			"remove",
+			!jq_block.hasClass("is_free") && !jq_block.hasClass("unavailable")
+		);
+	}
+
+
+	function enable_menu_action_if_true(action, t) {
+
+		var selector = "li[data-action='" + action + "']";
+		var elements = $("#block_menu").find(selector);
+
+		if(t) {
+			elements.removeClass("ui-state-disabled");
+		}
+		else {
+			elements.addClass("ui-state-disabled");
+		}
+	}
+
+
+	function handle_menu_selection(e, jq_element) {
+
+		switch(e.currentTarget.dataset.action) {
+
+			case "open" :
+				jq_block.find("a.link")[0].click();
+				break;
+				
+			case "place" :
+				place_block(
+					jq_block.data("block_id"),
+					e.currentTarget.dataset.block_type
+				);
+				break;
+		}
+
+	}
+
+
+	function place_block(block_id, block_type){
+
+		console.log("placing", block_type, "at block_id", block_id);
+		var url = '/api/frame/place_block';
+		var data = {
+			block_id: block_id,
+			block_type: block_type
+		};
+
+
+		$.ajax({
+			url: url,
+			type: "POST",
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify(data),
+			dataType: "json",
+			success: function(json) {
+				console.log("placed block ok");
+				jq_block.removeClass("is_free");
+				jq_block.find("span.name").first().text("");
+			},
+			error: function(xhr, status) {
+				var error_code = xhr.status + " " + xhr.statusText;
+				alert("ERROR placing block: " + error_code);
+			}
+		});
+	}
+
+
+
+
 
 	console.log("loaded frame.js");
 });
