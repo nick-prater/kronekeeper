096067c6529d59ae6d325bc8f55dedbd01a5d07f
Nick Prater <nick@npbroadcast.com>
2020-11-15 15:42:57 +0000

    Add roles for edit_jumper_templates and configure_block_types

---
+++ sql/changes/4.sql
@@ -0,0 +1,6 @@
+-- Schema changes v3 to v4
+INSERT INTO role (role, rank) VALUES ('configure_block_types', 500);
+INSERT INTO role (role, rank) VALUES ('configure_jumper_templates', 500);
+
+UPDATE kronekeeper_data SET value=4 WHERE key='db_version';
+
