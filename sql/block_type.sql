eb8c9688d9ded01a682031743ef387a57956957b
Nick Prater <nick@npbroadcast.com>
2020-11-17 11:42:39 +0000

    Prevent change of block_type pin and circuit count if in use
      * Added database constraints
      * Added `block_type_is_used()` database function
      * Extended `block_type_info` view

--- sql/block_type.sql
+++ sql/block_type.sql
@@ -19,6 +19,48 @@ along with Kronekeeper.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
+/* Returns true if the specified block type is in use, or false otherwise.
+ * Can be used to determine if a block type can be deleted.
+ */
+CREATE OR REPLACE FUNCTION block_type_is_used(
+	p_block_type_id INTEGER
+)
+RETURNS BOOLEAN AS $$
+BEGIN
+	IF EXISTS (SELECT 1 FROM block WHERE block.block_type_id = p_block_type_id) THEN
+		/* block_type is in use */
+		RETURN TRUE;
+	END IF;
+
+	/* Otherwise it must be unused */
+	RETURN FALSE;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION process_in_use_block_type_dimension_change()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF block_type_is_used(OLD.id) AND (
+		NEW.circuit_count != OLD.circuit_count OR
+		NEW.circuit_pin_count != OLD.circuit_pin_count
+	)
+	THEN RAISE EXCEPTION 'Cannot update block_type circuit_count or circuit_pin_count when block_type is in use';
+	END IF;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS catch_in_use_block_type_dimension_change ON block_type;
+CREATE TRIGGER catch_in_use_block_type_dimension_change
+BEFORE UPDATE OF circuit_count, circuit_pin_count
+ON block_type
+FOR EACH ROW
+EXECUTE PROCEDURE process_in_use_block_type_dimension_change();
+
+
+
 CREATE OR REPLACE VIEW block_type_info AS
 SELECT
 	block_type.id,
@@ -26,7 +68,8 @@ SELECT
 	block_type.name,
 	block_type.circuit_count,
 	block_type.circuit_pin_count,
-        CONCAT('#', ENCODE(block_type.colour_html_code, 'hex')) AS html_colour
+        CONCAT('#', ENCODE(block_type.colour_html_code, 'hex')) AS html_colour,
+	block_type_is_used(block_type.id) AS is_used
 FROM block_type;
 
 
